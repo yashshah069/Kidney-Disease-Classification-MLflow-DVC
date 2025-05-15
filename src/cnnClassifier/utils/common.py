8a7ab96b64ae773c71194d16d3d484c1d8ec3f11
Yash Shah <yashshah069@gmail.com>
2025-05-15 18:01:36 +0530

    Workflows added

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,24 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+@ensure_annotations
+
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yml file is empty")
+    except Exception as e:
+        raise e
